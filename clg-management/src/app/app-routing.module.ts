c08cbdf84ccf7830ea1f28e202e13be06c8d5384
Akshatakallur <akshatakallur10@gmail.com>
2023-06-23 13:12:50 +0530

    Second Commit

--- clg-management/src/app/app-routing.module.ts
+++ clg-management/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { FeesComponent } from './fees/fees.component';
+import { CourseComponent } from './course/course.component';
+import { AttendenceComponent } from './attendence/attendence.component';
+import { FacultyDetailsComponent } from './faculty-details/faculty-details.component';
+import { TimeTableComponent } from './time-table/time-table.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+const routes: Routes = [
+  
+  {path:'home',component:HomeComponent},
+  {path:'admission',component:AdmissionComponent},
+  {path:'fees',component:FeesComponent},
+  {path:'course',component:CourseComponent},
+  {path:'attendence',component:AttendenceComponent},
+  {path:'faculty',component:FacultyDetailsComponent},
+  {path:'timetable',component:TimeTableComponent},
+  {path:'feedback',component:FeedbackComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- clg-management/src/app/app.module.ts
+++ clg-management/src/app/app.module.ts
@@ -5,16 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { HomeComponent } from './home/home.component';
+import { FeesComponent } from './fees/fees.component';
+import { CourseComponent } from './course/course.component';
+import { FacultyDetailsComponent } from './faculty-details/faculty-details.component';
+
+import { TimeTableComponent } from './time-table/time-table.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { AttendenceComponent } from './attendence/attendence.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AdmissionComponent,
+    HomeComponent,
+    FeesComponent,
+    CourseComponent,
+    FacultyDetailsComponent,
+    TimeTableComponent,
+    FeedbackComponent,
+    AttendenceComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
